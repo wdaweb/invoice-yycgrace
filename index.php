aeaa8df259daa58c03c7df76743f5f6e3a9d5ca5
yycgrace <yycgrace84@gmail.com>
2020-05-08 14:24:05 +0800

    建立輸入發票流程

--- index.php
+++ index.php
@@ -7,6 +7,32 @@
     <link rel="stylesheet" href="./css/style.css">
 </head>
 <body>
-    <?php include "./include/header.php";?>
+<?php include "./include/header.php";?>
+
+<form action="save_invoice.php" method="post">
+    期別:
+    <select name="period">
+        <option value="1">1,2月</option>
+        <option value="2">3,4月</option>
+        <option value="3">5,6月</option>
+        <option value="4">7,8月</option>
+        <option value="5">9,10月</option>
+        <option value="6">11,12月</option>
+    </select>
+    年份:
+    <select name="year">
+        <option value="2020">2020</option>
+        <option value="2021">2021</option>
+        <option value="2022">2022</option>
+    </select>
+    獎號:
+    <input type="text" name="code">
+    <input type="text" name="number">
+    <input type="submit" value="儲存">
+    花費:
+    <input type="number" name="spend">
+    <input type="submit" value="儲存">
+</form>
+
 </body>
 </html>
---
+++ save_invoice.php
@@ -0,0 +1,9 @@
+<?php
+
+echo "period=".$_POST['period']."<br>";
+echo "period=".$_POST['year']."<br>";
+echo "period=".$_POST['code']."<br>";
+echo "period=".$_POST['number']."<br>";
+echo "period=".$_POST['spend']."<br>";
+
+?>
