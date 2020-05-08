9c2c3c8750810ce2ea227e75976f27babe287453
yycgrace <yycgrace84@gmail.com>
2020-05-08 13:37:07 +0800

    架構建置

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php include_once "base.php";?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?= include "title.php" ;?></title>
+    <link rel="stylesheet" href="./css/style.css">
+</head>
+<body>
+    <?php 
+    $pageheader="首頁";
+    include "./layout/topbar.php";?>
+</body>
+</html>
