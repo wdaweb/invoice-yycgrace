b062747adab840032530313138e232e614b2f929
yycgrace <yycgrace84@gmail.com>
2020-05-08 16:11:58 +0800

    新增資料庫檔案及發票輸入後跳轉選項

--- save_invoice.php
+++ save_invoice.php
@@ -9,10 +9,13 @@ $sql="insert into invoice (`period`,`year`,`code`,`number`,`expend`) values('".$
 $res=$pdo->exec($sql);
 
 if($res==1){
-    echo "新增成功";
+    echo "新增成功<br>";
+    // header('location:index.php');
 }else{
-    echo "新增失敗";
+    echo "新增失敗<br>";
 }
+echo "<a href='index.php'>繼續輸入</a><br>";
+echo "<a href='list.php'>發票列表</a><br>";
 
 
 ?>
